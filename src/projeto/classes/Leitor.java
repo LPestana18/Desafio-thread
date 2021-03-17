1904d284f7b2d2bd5fc99c753a83ea16d9684dfa
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2021-03-17 09:30:51 -0300

    Versão 1.0 do projeto

--- src/projeto/classes/Leitor.java
+++ src/projeto/classes/Leitor.java
@@ -4,30 +4,30 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Leitor implements Runnable {
 
-    @Override
-    public void run() {
+    private String path;
+    private String index;
 
-        File file = new File("temp");
-        File[] arquivos = file.listFiles();
-        for (int i = 0; i < arquivos.length; i++) {
-
-            System.out.println(arquivos[i].getName());
-        }
-
-        System.out.println("Lendo ArquivoA");
-        lerArquivo("temp\\Arquivo.txt", "a");
-        System.out.println("Lendo ArquivoB");
-        lerArquivo("temp\\ArquivoB.txt", "b");
+    public Leitor(String path, String index) {
+        this.path = path;
+        this.index = index;
     }
 
-    public static synchronized void lerArquivo(String pathArquivo, String index) {
+    @Override
+    public void run() {
+        lerArquivo(path, index);
+    }
+
+    public static synchronized  void lerArquivo(String pathArquivo, String index) {
         File file = new File(pathArquivo);
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String s;
+            System.out.println(Thread.currentThread().getName());
             while ((s = br.readLine()) != null) {
                 if (s.endsWith(index)) {
                     System.out.println(s);
@@ -37,5 +37,32 @@ public class Leitor implements Runnable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public List<String> listaDeArquivos() {
+        File file = new File("temp");
+        File[] arquivos = file.listFiles();
+        List<String> nomeArquivos = new ArrayList<>();
+        for (File arquivo : arquivos) {
+            nomeArquivos.add(arquivo.getPath());
+        }
+
+        return nomeArquivos;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
     }
 }
--- src/projeto/program/Main.java
+++ src/projeto/program/Main.java
@@ -2,28 +2,16 @@ package projeto.program;
 
 import projeto.classes.Leitor;
 
-import java.io.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        Leitor leitor = new Leitor();
-        leitor.run();
-    }
+        Leitor leitor1 = new Leitor("temp\\Arquivo.txt", "a");
+        Thread leitorArquivoA = new Thread(leitor1, "Arquivo A");
+        leitorArquivoA.start();
 
+        Leitor leitor2 = new Leitor("temp\\ArquivoB.txt", "b");
+        Thread leitorArquivoB = new Thread(leitor2, "Arquivo B");
+        leitorArquivoB.start();
 
-    public static void lerArquivo(String pathArquivo, String index) {
-        File file = new File(pathArquivo);
-
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String s;
-            while ((s = br.readLine()) != null) {
-                if (s.endsWith(index)) {
-                    System.out.println(s);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
